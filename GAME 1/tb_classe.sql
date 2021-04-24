e48b82ccca822f34a0e74e949e3ac882c2017101
IsacBianor <80279042+IsacBianor@users.noreply.github.com>
2021-04-23 22:26:15 -0300

    Add files via upload

---
+++ GAME 1/tb_classe.sql
@@ -0,0 +1,5 @@
+SELECT * FROM db_generation_game_online.tb_classe;
+
+INSERT INTO tb_classe (nome, aereo, terrestre, aquatico) VALUES ("Cachorro", false, true, false),
+("Sapo", false, true, true), ("Gato", false, true, false),
+("Urubu", true, false, false), ("Ganso", true, true, true)
---
+++ GAME 1/tb_personagem.sql
@@ -0,0 +1,5 @@
+SELECT * FROM db_generation_game_online.tb_personagem;
+
+INSERT INTO tb_personagem (nome, ataque, defesa, velocidade, resistencia, tb_classe_id_classe) VALUES
+("Titi", 50000, 30, 50, 2000, 1), ("Liz", 95, 95, 20, 100, 2), ("Carol", 80, 90, 80, 100, 1),
+("Edu", 50, 75, 75, 100, 3), ("Isac", 80, 80, 40, 100, 5)
